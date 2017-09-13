fc4fda39342e192ca2760e835aacc6eef0e8b3b3
Me <goodisgood153@gmail.com>
2017-09-13 16:35:09 +0800

    Edit Playlist Info

--- playlist/urls.py
+++ playlist/urls.py
@@ -4,5 +4,6 @@ from playlist import views
 
 urlpatterns = [
 url(r'^$', views.PlaylistsView.as_view(), name='playlists'),
+url(r'^(?P<playlist_id>\d+)/details/$',views.PlaylistDetail.as_view(),name='playlist_detail'),
 url(r'^(?P<playlist_id>\d+)/delete/$',views.PlaylistDelete.as_view(),name='delete_playlist'),
 ]
--- playlist/views.py
+++ playlist/views.py
@@ -6,27 +6,51 @@ from playlist.forms import PlaylistForm, SongForm
 
 
 class PlaylistsView(TemplateView):
-    """List Playlists && Create Playlist
+    """List Playlists and Create Playlist
     """
 
     template_name = "playlist/playlists.html"
     form = PlaylistForm
 
-    def get(self,*args,**kwargs):
+    def get(self, *args, **kwargs):
         playlists = Playlist.objects.all().order_by('-date_created')
         context = {'playlists':playlists,
                    'form':self.form}
-        return render(self.request,self.template_name,context)
+        return render(self.request, self.template_name, context)
 
-    def post(self,*args,**kwargs):
+    def post(self, *args, **kwargs):
         form = self.form(self.request.POST)
         form.save()
         return redirect('playlists')
 
 
+class PlaylistDetail(TemplateView):
+    """ Playlist View Detail and Edit Playlist Info
+    """
+
+    template_name = "playlist/playlist_detail.html"
+    form = PlaylistForm
+
+    def get(self, *args, **kwargs):
+        playlist_id = kwargs['playlist_id']
+        playlist = Playlist.objects.get(id=playlist_id)
+        form = self.form(instance = playlist)
+        context = {'form':form, 'playlist':playlist}
+        return render(self.request, self.template_name, context)
+
+    def post(self, *args, **kwargs):
+        playlist_id = kwargs['playlist_id']
+        playlist = Playlist.objects.get(id=playlist_id)
+        form = self.form(self.request.POST, instance = playlist)
+        if form.is_valid():
+            form.save()
+        return redirect('playlists')
+
+
 class PlaylistDelete(View):
 
-        def get(self,*args,**kwargs):
+        def get(self, *args, **kwargs):
             playlist_id = kwargs['playlist_id']
+            playlist = Playlist.objects.get(id=playlist_id)
             Playlist.objects.get(id= playlist_id).delete()
             return redirect('playlists')
