e4831f1e1c17d2a987e3fb41a03b050c963ef561
Me <goodisgood153@gmail.com>
2017-09-15 11:54:17 +0800

    Delete Song

--- playlist/urls.py
+++ playlist/urls.py
@@ -3,5 +3,6 @@ from django.conf.urls import url
 from playlist import views
 
 urlpatterns = [
-    url(r'^playlist/(?P<playlist_id>\d+)/',views.PlaylistView.as_view(),name ="playlist")
+    url(r'^playlist/(?P<playlist_id>\d+)/$',views.PlaylistView.as_view(),name ="playlist"),
+    url(r'^playlist/(?P<playlist_id>\d+)/(?P<song_id>\d+)/delete/$',views.SongDelete.as_view(),name ="song_delete"),
 ]
