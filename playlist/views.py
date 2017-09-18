3555edc652f07efa99eb9d0e9973bf66f1511d9a
Me <goodisgood153@gmail.com>
2017-09-18 18:52:16 +0800

    Edit Validation

--- playlist/views.py
+++ playlist/views.py
@@ -1,6 +1,7 @@
 from django.views.generic import TemplateView, View
 from django.shortcuts import render, redirect
 from django.shortcuts import get_object_or_404
+from django.http import Http404
 
 from playlist.models import Playlist, Song
 from users.models import User
@@ -11,94 +12,97 @@ class PlaylistsView(TemplateView):
     """ViewPlaylists && Add PLAYLIST
     """
     template_name = 'playlist/playlists.html'
-    form = PlaylistForm
 
     def get(self, *args, **kwargs):
         """show all playlist
         """
-        context = {
+        return render(self.request, self.template_name, {
             'playlists': Playlist.objects.all(),
-            'form': self.form,
-        }
-        return render(self.request, self.template_name, context)
+            'form': PlaylistForm(),
+        })
 
     def post(self, *args, **kwargs):
         """create playlist
         """
-        form = self.form(self.request.POST)
+        form = PlaylistForm(self.request.POST)
         if form.is_valid():
             form.save()
             return redirect('playlists')
-        context = {
+        return render(self.request, self.template_name, {
             'playlists': Playlist.objects.all(),
             'form': form,
-        }
-        return render(self.request, self.template_name, context)
+        })
 
 
 class PlaylistView(TemplateView):
     """ViewPlaylist && Add SONG
     """
     template_name = 'playlist/playlist.html'
-    form = SongForm
 
     def get(self, *args, **kwargs):
         """show all songs from playlist
         """
         playlist = get_object_or_404(Playlist, id=kwargs['playlist_id'])
         songs = Song.objects.filter(playlist=playlist)
-        context={
+        return render(self.request, self.template_name, {
             'playlist': playlist,
-            'form': self.form,
+            'form': SongForm(),
             'songs': songs,
-        }
-        return render(self.request, self.template_name, context)
+        })
 
     def post(self,*args,**kwargs):
         """add song to playlist
         """
         playlist = get_object_or_404(Playlist, id=kwargs['playlist_id'])
         songs = Song.objects.filter(playlist=playlist)
-        form = self.form(self.request.POST)
+        form = SongForm(self.request.POST)
         if form.is_valid():
             form.instance.playlist = playlist
             form.instance.user = self.request.user
             form.save()
             return redirect('playlist', kwargs['playlist_id'])
-        context={
+        return render(self.request, self.template_name, {
             'playlist': playlist,
             'form': form,
             'songs': songs,
-        }
-        return render(self.request, self.template_name, context)
+        })
 
 
 class SongDetail(View):
-    """ View song && edit song from playlist
+    """ View song && edit song from playlists
     """
     template_name = 'playlist/songdetail.html'
-    form = SongForm
 
     def get(self, *args, **kwargs):
         """show song details
         """
         playlist = get_object_or_404(Playlist, id=kwargs['playlist_id'])
         song = get_object_or_404(Song, id=kwargs['song_id'])
-        form = self.form(instance=song)
-        context = {
-            'form': form,
-            'playlist': playlist,
-        }
-        return render(self.request, self.template_name, context)
+        form = SongForm(instance=song)
+        permission = False
+        if self.request.user == song.user:
+            permission = True
+        return render(self.request, self.template_name, {
+                'permission': permission,
+                'form': form,
+                'playlist': playlist,
+        })
 
     def post(self, *args, **kwargs):
         """update song details
         """
         song = get_object_or_404(Song, id=kwargs['song_id'])
-        if self.request.user == song.user:
-            form = self.form(self.request.POST,instance=song)
-            form.save()
-        return redirect('playlist',kwargs['playlist_id'])
+        form = SongForm(self.request.POST,instance=song)
+        if form.is_valid():
+            if self.request.user == song.user:
+                form.save()
+                return redirect('playlist',kwargs['playlist_id'])
+            else:
+                raise Http404("User does not have permission to edit the song.")
+        return render(self.request, self.template_name, {
+            'playlist': get_object_or_404(Playlist, id=kwargs['playlist_id']),
+            'form':form,
+        })
 
 
 class SongDelete(View):
