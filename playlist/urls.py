03590d853b1a2ebf1f76bacfdd42896f79dad1ca
Me <goodisgood153@gmail.com>
2017-09-18 15:17:53 +0800

    Edit Song

--- playlist/urls.py
+++ playlist/urls.py
@@ -3,7 +3,8 @@ from django.conf.urls import url
 from playlist import views
 
 urlpatterns = [
-    url(r'^playlists/$', views.PlaylistsView.as_view(), name ="playlists"),
+    url(r'^$', views.PlaylistsView.as_view(), name ="playlists"),
     url(r'^playlist/(?P<playlist_id>\d+)/$', views.PlaylistView.as_view(), name ="playlist"),
+    url(r'^playlist/(?P<playlist_id>\d+)/(?P<song_id>\d+)/$', views.SongDetail.as_view(), name ="song_detail"),
     url(r'^playlist/(?P<playlist_id>\d+)/(?P<song_id>\d+)/delete/$', views.SongDelete.as_view(), name ="song_delete"),
 ]
