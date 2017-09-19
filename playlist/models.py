25e48f000c62e86316305af45d6f1eb892400c2b
cholorecto <pocholorecto123@gmail.com>
2017-09-19 14:45:36 +0800

    added song history 'added and updated'

--- playlist/models.py
+++ playlist/models.py
@@ -27,3 +27,36 @@ class Song(models.Model):
 
     def __str__(self):
         return "{}".format(self.title)
+
+    def save(self, *args, **kwargs):
+        """ Override the save function to 
+            add song history
+        """
+        if self.id:
+            # the user edited the added song
+            log = SongHistory.objects.create(user=self.user,
+                                             title=self.title,
+                                             link=self.link,
+                                             action='Updated')
+            log.save()
+        else:
+            # the user added song
+            log = SongHistory.objects.create(user=self.user,
+                                             title=self.title,
+                                             link=self.link,
+                                             action='Added')
+            log.save()
+        super(Song, self).save(*args, **kwargs) # Calls the real save method
+
+
+class SongHistory(models.Model):
+    """ Tracks action done on Song model 
+    """
+    user = models.ForeignKey(User)
+    title = models.CharField(max_length=255)
+    link = models.URLField()
+    action = models.CharField(max_length=16)
+    date = models.DateTimeField(auto_now_add=True) 
+
+    def __str__(self):
+        return "{}-{}".format(self.title, self.action)
