3daba4098cdbd55627edb9b3e694ab91fec58f90
Me <goodisgood153@gmail.com>
2017-09-19 15:45:24 +0800

    Updates/Fixes

--- playlist/models.py
+++ playlist/models.py
@@ -4,7 +4,7 @@ from users.models import User
 
 
 class Playlist(models.Model):
-    """Songlist
+    """Playlist
     """
     title = models.CharField(max_length=255)
     description = models.TextField(blank=True)
--- playlist/urls.py
+++ playlist/urls.py
@@ -1,10 +1,10 @@
 from django.conf.urls import url
 
-from playlist import views
+from .views import PlaylistsView, PlaylistView, SongDetail, SongDelete
 
 urlpatterns = [
-    url(r'^$', views.PlaylistsView.as_view(), name ="playlists"),
-    url(r'^playlist/(?P<playlist_id>\d+)/$', views.PlaylistView.as_view(), name ="playlist"),
-    url(r'^playlist/(?P<playlist_id>\d+)/(?P<song_id>\d+)/$', views.SongDetail.as_view(), name ="song_detail"),
-    url(r'^playlist/(?P<playlist_id>\d+)/(?P<song_id>\d+)/delete/$', views.SongDelete.as_view(), name ="song_delete"),
+    url(r'^$', PlaylistsView.as_view(), name ="playlists"),
+    url(r'^playlist/(?P<playlist_id>\d+)/$', PlaylistView.as_view(), name ="playlist"),
+    url(r'^playlist/(?P<playlist_id>\d+)/(?P<song_id>\d+)/$', SongDetail.as_view(), name ="song_detail"),
+    url(r'^playlist/(?P<playlist_id>\d+)/(?P<song_id>\d+)/delete/$', SongDelete.as_view(), name ="song_delete"),
 ]
