7f3764472d85d9c665bc09e0932b2fd46c2ec631
cholorecto <pocholorecto123@gmail.com>
2017-09-19 15:28:08 +0800

    cleaned up the codes and convert the action field into dropdown

--- playlist/models.py
+++ playlist/models.py
@@ -32,31 +32,37 @@ class Song(models.Model):
         """ Override the save function to 
             add song history
         """
+        log = SongHistory.objects.create(user=self.user,
+                                         title=self.title,
+                                         link=self.link)
         if self.id:
-            # the user edited the added song
-            log = SongHistory.objects.create(user=self.user,
-                                             title=self.title,
-                                             link=self.link,
-                                             action='Updated')
-            log.save()
+            log.action = SongHistory.UPDATED
         else:
-            # the user added song
-            log = SongHistory.objects.create(user=self.user,
-                                             title=self.title,
-                                             link=self.link,
-                                             action='Added')
-            log.save()
+            log.action = SongHistory.ADDED
+        log.save()
+
         super(Song, self).save(*args, **kwargs) # Calls the real save method
 
 
 class SongHistory(models.Model):
     """ Tracks action done on Song model 
     """
+    ADDED = 'Added'
+    UPDATED = 'Updated'
+    DELETED = 'Deleted'
+    
+    actions = (
+        (ADDED, 'Added'),
+        (UPDATED, 'Updated'),
+        (DELETED, 'Deleted'),
+    )
     user = models.ForeignKey(User)
     title = models.CharField(max_length=255)
     link = models.URLField()
-    action = models.CharField(max_length=16)
+    action = models.CharField(max_length=16, choices=actions)
     date = models.DateTimeField(auto_now_add=True) 
 
     def __str__(self):
         return "{}-{}".format(self.title, self.action)
+
+
