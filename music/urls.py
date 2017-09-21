cc73d482ad10ca82c904ae92ca923b6f6b8fdbd1
cholorecto <pocholorecto123@gmail.com>
2017-09-21 12:23:25 +0800

    added index.html

--- music/urls.py
+++ music/urls.py
@@ -18,10 +18,12 @@ from django.contrib import admin
 
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import TemplateView
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^', include('users.urls')),
     url(r'^videoplayer/', include('videoplayer.urls')),
     url(r'^music/',include('playlist.urls')),
+    url(r'^$', TemplateView.as_view(template_name='index.html'))
 ]
