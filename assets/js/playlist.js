43bac58105394151a4674ada27fc799aca9100c6
cholorecto <pocholorecto123@gmail.com>
2017-10-05 12:22:47 +0800

    indentions in playlist.js

--- assets/js/playlist.js
+++ assets/js/playlist.js
@@ -42,7 +42,7 @@
   });
 
     $(document).on('submit', '#search_playlist', function(event){
-    event.preventDefault();
+      event.preventDefault();
     $.ajax({
       type: 'POST',
       url: $(this).attr('action-url'),
@@ -53,7 +53,7 @@
   });
 
     $(document).on('click', '#all_playlist', function(event){
-    event.preventDefault();
+      event.preventDefault();
     $.ajax({
       type: 'Get',
       url: $(this).attr('url'),
